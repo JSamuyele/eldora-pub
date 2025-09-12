a3f3ad2ee599e93f218c87c9f05d954646c975f0
JSamuyele <jacobsamuyele@gmail.com>
2025-09-12 21:54:32 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       server: {
         proxy: {
           '/api': {
-            target: 'http://localhost:8000',
+            target: 'https://www.desklearn.com',
             changeOrigin: true,
             secure: false,
           },
