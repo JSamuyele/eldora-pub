b7c1ec17a26a2b2c24c75f708c14524905b67b92
JSamuyele <jacobsamuyele@gmail.com>
2025-09-13 19:32:04 +0000

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://www.desklearn.com/api", // production backend
+  baseURL: "https://api.desklearn.com/api", // production backend
   withCredentials: true,
 });
 
