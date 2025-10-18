5fcd1f83eaa586b4c552a514249deba1694ab850
JSamuyele <jacobsamuyele@gmail.com>
2025-10-18 22:34:49 +0000

    Delete src directory

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-
-
-export { default as Auth } from './Auth.jsx'
-
-export { default as Tables } from './Tables';
-export { default as Sales } from './Sales';
-export { default as Events } from './Events';
-export { default as Dashboard } from "./Dashboard.jsx"
-export { default as Inventory } from "./Inventory.jsx"
