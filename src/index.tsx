a8484f33e643db92bd7e3ad18c006f3a272f3873
JSamuyele <jacobsamuyele@gmail.com>
2025-10-26 06:02:39 +0000

    Replace HashRouter with BrowserRouter in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-
 // pos-frontend/src/index.tsx
 
 import React from 'react';
@@ -6,33 +5,33 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SnackbarProvider } from 'notistack';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // *** FIX IS HERE ***
 import { store } from './redux/store';
 import App from './App';
-import './index.css'; // Import Tailwind CSS base styles
+import './index.css';
 
 const queryClient = new QueryClient();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <SnackbarProvider 
-          maxSnack={3} 
-          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-          autoHideDuration={3000}
-        >
-          <HashRouter>
-            <App />
-          </HashRouter>
-        </SnackbarProvider>
-      </QueryClientProvider>
-    </Provider>
-  </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <SnackbarProvider 
+          maxSnack={3} 
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          autoHideDuration={3000}
+        >
+          <BrowserRouter> {/* *** AND HERE *** */}
+            <App />
+          </BrowserRouter>
+        </SnackbarProvider>
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
 );
